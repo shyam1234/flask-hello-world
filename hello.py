191c5bf7eb02369eee5fa0e3a436592062281957
Prafulla Malviya <malviya.software@gmail.com>
2020-12-05 19:34:45 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello Azure!</h1>"
+def hello():
+    return "Hello, World!"
